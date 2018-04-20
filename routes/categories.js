866e46f5be26a658b404c5666b367f7ddb972528
vbeakovic <valter.beakovic@gmail.com>
2018-04-20 20:36:41 +0200

    Category CRUD functionality completed

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
+Category = require('../models/category.js');
+
+// Get categories - GET
 router.get('/', (req, res, next) => {
-  res.render('categories', {
-    title: 'Categories'
+  Category.getCategories((err, categories) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render('categories', {
+      title: 'Categories',
+      categories: categories
+    });
+  });
+});
+
+// Add category - POST
+router.post('/add', (req, res, next) => {
+  let category = new Category();
+  category.title = req.body.title;
+  category.description = req.body.description;
+  Category.addCategory(category, (err, category) => {
+    if (err) {
+      res.send(err);
+    }
+    res.redirect('/manage/categories');
+  });
+});
+
+// Edit category - POST
+router.post('/edit/:id', (req, res, next) => {
+  let category = new Category();
+  const query = {_id: req.params.id};
+
+  const update = { title: req.body.title, description: req.body.description};
+  Category.updateCategory(query, update, {}, (err, category) => {
+    if (err) {
+      res.send(err);
+    }
+    res.redirect('/manage/categories');
+  });
+});
+
+// Delete category - DELETE
+router.delete('/delete/:id', (req, res, next) => {
+  const query = {_id: req.params.id};
+
+  Category.removeCategory(query, (err, category) => {
+    if (err) {
+      res.send(err);
+          console.log('Test2');
+    }
+    res.sendStatus(200);
+    console.log('Test');
   });
 });
 
