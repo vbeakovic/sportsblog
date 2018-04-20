23905f6b78748db36448ba8ba1071d92c037b3a6
vbeakovic <valter.beakovic@gmail.com>
2018-04-20 20:34:57 +0200

    Ajax request to delete category

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  $('.category-delete').on('click', (e) => {
+    $target = $(e.target);
+    const id = $target.attr('data-cat-id');
+    $.ajax({
+      type: 'DELETE',
+      url: '/categories/delete/' + id,
+      success: (response) => {
+        //alert('Deleting Todo');
+        console.log(response);
+        //$("#getCode").html(response);
+        //$("#getCodeModal").modal('show');
+        //window.location.href = '/';
+        //$('.alert').show();
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  });
+  //$('#getCodeModal').on('hidden.bs.modal', function (e) {
+  //  window.location.href = '/manage/categories';
+});
+// $('.alert .close').on('click', function(e) {
+//     $(this).parent().hide();
+//     window.location.href = '/manage/categories';
+// });
