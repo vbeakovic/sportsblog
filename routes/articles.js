ba33411bb4697ca4f9b936ef9665ac86984e8c62
vbeakovic <valter.beakovic@gmail.com>
2018-04-21 19:12:39 +0200

    Article add route handled

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+Article = require('../models/article.js');
+
 router.get('/', (req, res, next) => {
   res.render('articles', {
     title: 'Articles'
@@ -16,6 +18,22 @@ router.get('/show/:id', (req, res, next) => {
 router.get('/category/:category_id', (req, res, next) => {
   res.render('articles', {
     title: 'Category Articles'
+  });
+});
+
+// Add article - POST
+router.post('/add', (req, res, next) => {
+  let article = new Article();
+  article.title = req.body.title;
+  article.subtitle = req.body.subtitle;
+  article.category = req.body.category;
+  article.body = req.body.body;
+  article.author = req.body.author;
+  Article.addArticle(article, (err, category) => {
+    if (err) {
+      res.send(err);
+    }
+    res.redirect('/manage/articles');
   });
 });
 
