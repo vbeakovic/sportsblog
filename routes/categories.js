d7071f42fe787b6dd9fc7c875cd7b7646f6a390f
vbeakovic <valter.beakovic@gmail.com>
2018-04-25 20:23:44 +0200

    Console log removed

--- routes/categories.js
+++ routes/categories.js
@@ -26,7 +26,6 @@ router.post('/add', [
 ], (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.mapped());
     res.render('add_category', {
       title: 'Create Category',
       category_title: req.body.title,
