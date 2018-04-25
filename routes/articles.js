51c7589e29b6118378c5f4c8719a6bac739ead22
vbeakovic <valter.beakovic@gmail.com>
2018-04-25 20:58:24 +0200

    Add article validation completed

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
 
+Category = require('../models/category.js');
 Article = require('../models/article.js');
 
 router.get('/', (req, res, next) => {
@@ -22,19 +25,43 @@ router.get('/category/:category_id', (req, res, next) => {
 });
 
 // Add article - POST
-router.post('/add', (req, res, next) => {
-  let article = new Article();
-  article.title = req.body.title;
-  article.subtitle = req.body.subtitle;
-  article.category = req.body.category;
-  article.body = req.body.body;
-  article.author = req.body.author;
-  Article.addArticle(article, (err, category) => {
-    if (err) {
-      res.send(err);
-    }
-    res.redirect('/manage/articles');
-  });
+router.post('/add', [
+  check('title').not().isEmpty().withMessage('The title cannot be empty'),
+  check('subtitle').not().isEmpty().withMessage('The subtitle cannot be empty'),
+  check('author').not().isEmpty().withMessage('The author cannot be empty'),
+  check('body').not().isEmpty().withMessage('The body cannot be empty')
+], (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    Category.getCategories((err, categories) => {
+      if (err) {
+        console.log(err);
+      }
+      res.render('add_article', {
+        title: 'Create Article',
+        article_title: req.body.title,
+        article_subtitle: req.body.subtitle,
+        article_author: req.body.author,
+        article_category: req.body.category,
+        article_body: req.body.body,
+        categories: categories,
+        error_message: errors.mapped()
+      });
+    });
+  } else {
+    let article = new Article();
+    article.title = req.body.title;
+    article.subtitle = req.body.subtitle;
+    article.category = req.body.category;
+    article.body = req.body.body;
+    article.author = req.body.author;
+    Article.addArticle(article, (err, category) => {
+      if (err) {
+        res.send(err);
+      }
+      res.redirect('/manage/articles');
+    });
+  }
 });
 
 // Update article - POST
